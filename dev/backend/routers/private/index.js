d6dae03228214b6a6dc50da7760e637f6f8cc5d2
reginaldo melo <reginaldo.melo56@gmail.com>
2020-08-26 18:12:16 -0300

    adicionar as rotas que falta referente aos heroes e adicione o middleware para validação em algumas

--- dev/backend/routers/private/index.js
+++ dev/backend/routers/private/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router=express.Router();
 const heroController = require('../../controllers/heroes');
+const herosControllerMiddlewareValidation = require('../../middleware/controllers-validation/heroes');
 
-router.post('/hero',heroController.newHero);
+router.post('/hero',herosControllerMiddlewareValidation.newHero,heroController.newHero);
+router.get('/hero',herosControllerMiddlewareValidation.listHeroes,heroController.listHeroes);
+router.put('/hero/:id_hero',herosControllerMiddlewareValidation.updateHero,heroController.updateHero);
+router.get('/hero/:id_hero',heroController.getOneHero);
+router.delete('/hero/:id_hero',heroController.deleteHero);
+
 
 module.exports=router; 
