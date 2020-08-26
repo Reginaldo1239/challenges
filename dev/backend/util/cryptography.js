89c79ab2ac4210e232fdf6e09017e20daf7ff392
reginaldo melo <reginaldo.melo56@gmail.com>
2020-08-26 01:12:07 -0300

    funcoes de criação e verificação de token jwt criadas

---
+++ dev/backend/util/cryptography.js
@@ -0,0 +1,37 @@
+exports.gerarTokenJwt = (objectWithValues)=>{
+    console.log(objectWithValues)
+     return new Promise((resolver,reject)=>{
+   try{      
+          jwt.sign(objectWithValues, 'privatssseKey', { algorithm: 'HS512' }, function(err, token) {
+            if(err){
+                console.log(err)
+                resolver(false);
+            }else{
+                resolver(token);
+            } 
+            });
+    }catch(e){
+            reject(false);
+            console.log(e);
+    }
+    
+     })
+}
+
+ exports.verificarTokenJwt = (token)=>{
+  return  new Promise((resolver,reject)=>{
+    try{
+            jwt.verify(token, 'privatssseKey' , { algorithms: ['HS512'] }, function (err, payload) {
+            if(err){
+                console.log(err);
+                resolver(false)
+            }
+            resolver(payload)
+            });
+    }catch{
+            console.log(e);
+            reject(false)
+    }
+       })
+    
+}
