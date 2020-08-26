5063b9f20a06d9a51f46af561a2464d50aebffaf
reginaldo melo <reginaldo.melo56@gmail.com>
2020-08-26 00:14:56 -0300

    model de heroes criado com crud finalizado

---
+++ dev/backend/models/heroes.js
@@ -0,0 +1,30 @@
+const db = require('../db/index');
+
+exports.newHero= async (values)=>{
+    const TABLE = 'heroes';
+    let {id_usuario,nome,classe,lat,lng}= values;
+
+    let post={id_usuario,nome,classe,lat,lng};
+    return await db.insert(TABLE,post);
+}
+exports.updatedHero = async (values)=>{
+    let {id_usuario,nome,classe,lat,lng} =values;
+ //   'UPDATE users SET foo = ?, bar = ?, baz = ? WHERE id = ?', ['a', 'b', 'c'
+    let query = "UPDATE heroes SET nome= ?, classe = ? , lat = ? ,lgn = ? WHERE id_usuario=? LIMIT 1";
+    let valuesOfQuery = [nome,classe,lat,lng,id_usuario];
+    return await db.update(query,valuesOfQuery);
+}
+
+exports.listHeroes = async (values)=>{
+    let {id_usuario,limit,page} = values
+    let query ="SELECT * FROM heroes WHERE id_usuario=? LIMIT ? OFFSET ?";
+    let valuesOfQuery=[id_usuario,parseInt(limit),page*page];
+    return await db.select(query,valuesOfQuery);
+}
+
+exports.deleteHero = async = values =>{
+    let {id_usuario,id_hero}  =values;
+    let query = `DELETE * FROM heroes WHERE id_usuario=${id_usuario} AND id_hero=${id_hero}`;
+    return await db.delete(query);
+}
+
