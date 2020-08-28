bf040acd658c165ce1a8489d2aa1794dc92d1bff
reginaldo melo <reginaldo.melo56@gmail.com>
2020-08-28 02:03:39 -0300

    adicionei uma função para  verificar  se o valor é um número

--- dev/backend/util/validacao.js
+++ dev/backend/util/validacao.js
@@ -8,3 +8,46 @@ exports.minLength=(value,minLength)=>{
         return false;
     } 
 } 
+exports.maxLength=(value,maxLength)=>{
+    if(typeof value=='string'){
+        if(value.length<=maxLength){
+            return true
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+} 
+exports.classeHero = (classe)=>{
+    let classeValida = false;
+
+    switch(classe){
+        case "S":
+            classeValida = true;
+            break;
+        case "A":
+            classeValida = true
+            break;
+        case "B":
+            classeValida = true;
+            break;
+        case "C" :
+            classeValida = true;
+            break;    
+    }
+ 
+    return classeValida;     
+}
+
+exports.isNumber  = value=>{
+    if(/^\d*$/.test(value)){
+        return true;
+    }else{ 
+        return false;
+    }
+
+}
+
+ 
+  
